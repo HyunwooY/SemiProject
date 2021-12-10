2b524b9bc0f23a4aaf4f8e44a27a85df078502c3
lamgu <lamgu@lamgul>
2021-12-10 13:28:36 +0900

    search_resultdao 업데이트

--- semiProject/src/main/java/chaneloper/dao/Search_ResultDao.java
+++ semiProject/src/main/java/chaneloper/dao/Search_ResultDao.java
@@ -31,23 +31,23 @@ public class Search_ResultDao {
 						+ "INNER JOIN PRODUCT_DETAIL b ON(a.PI_NUM = b.PI_NUM) "
 						+ "INNER JOIN PRODUCT_PHOTO c ON(a.PI_NUM = c.PI_NUM) "
 						+ "INNER JOIN TAG d ON(a.PI_NUM = d.PI_NUM) "
-						+ "WHERE a.PI_NAME like "+"%"+keyword+"% "
+						+ "WHERE a.PI_NAME like "+"\'%"+keyword+"%\' "
 						+ "ORDER BY a." + sort;
 			}else if(sort==null) {
-				sql = "SELECT a.PI_NUM ,a.PI_NAME,a.PI_PRICE, b.PD_COLOR,c.PP_TITLE, d.t_name FROM PRODUCT_INFOMATION a"
-						+ "INNER JOIN PRODUCT_DETAIL b ON(a.PI_NUM = b.PI_NUM)"
-						+ "INNER JOIN PRODUCT_PHOTO c ON(a.PI_NUM = c.PI_NUM)"
-						+ "INNER JOIN TAG d ON(a.PI_NUM = d.PI_NUM)"
-						+ "WHERE a.PI_NAME like "+"%"+keyword+"%"
+				sql = "SELECT a.PI_NUM ,a.PI_NAME,a.PI_PRICE, b.PD_COLOR,c.PP_TITLE, d.t_name FROM PRODUCT_INFOMATION a "
+						+ "INNER JOIN PRODUCT_DETAIL b ON(a.PI_NUM = b.PI_NUM) "
+						+ "INNER JOIN PRODUCT_PHOTO c ON(a.PI_NUM = c.PI_NUM) "
+						+ "INNER JOIN TAG d ON(a.PI_NUM = d.PI_NUM) "
+						+ "WHERE a.PI_NAME like "+"\'%"+keyword+"%\' "
 						+ "AND a.PI_CATEGORY = "+ CATEGORY;
 			}else if(CATEGORY!=null&&sort!=null) {
-				sql = "SELECT a.PI_NUM ,a.PI_NAME,a.PI_PRICE, b.PD_COLOR,c.PP_TITLE, d.t_name FROM PRODUCT_INFOMATION a"
-						+ "INNER JOIN PRODUCT_DETAIL b ON(a.PI_NUM = b.PI_NUM)"
-						+ "INNER JOIN PRODUCT_PHOTO c ON(a.PI_NUM = c.PI_NUM)"
-						+ "INNER JOIN TAG d ON(a.PI_NUM = d.PI_NUM)"
-						+ "WHERE a.PI_NAME like "+"%"+keyword+"%"
+				sql = "SELECT a.PI_NUM ,a.PI_NAME,a.PI_PRICE, b.PD_COLOR,c.PP_TITLE, d.t_name FROM PRODUCT_INFOMATION a "
+						+ "INNER JOIN PRODUCT_DETAIL b ON(a.PI_NUM = b.PI_NUM) "
+						+ "INNER JOIN PRODUCT_PHOTO c ON(a.PI_NUM = c.PI_NUM) "
+						+ "INNER JOIN TAG d ON(a.PI_NUM = d.PI_NUM) "
+						+ "WHERE a.PI_NAME like "+"\'%"+keyword+"%\' "
 						+ "AND a.PI_CATEGORY = "+ CATEGORY
-						+ "ORDER BY a." + sort;
+						+ " ORDER BY a." + sort;
 			}
 			pstmt=con.prepareStatement(sql);
 			rs = pstmt.executeQuery();
