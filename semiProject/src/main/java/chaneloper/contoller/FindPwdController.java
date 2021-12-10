8420caadc115068e0ef5c33e6a16b7218ca88535
slsl7 <slsl7@DESKTOP-C41FD0M>
2021-12-10 18:40:54 +0900

    login.jsp a태그 추가

--- semiProject/src/main/java/chaneloper/contoller/FindPwdController.java
+++ semiProject/src/main/java/chaneloper/contoller/FindPwdController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 
 import chaneloper.dao.MemberDao;
+import chaneloper.dao.SellerDao;
 @WebServlet("/pwd/find")
 public class FindPwdController extends HttpServlet{
 	@Override
@@ -21,19 +22,35 @@ public class FindPwdController extends HttpServlet{
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String id = req.getParameter("mi_id");
-		String email = req.getParameter("mi_email");
-		MemberDao dao = MemberDao.getInstance();
-		String pwd = dao.findPwd(id, email);
-		JSONObject json = new JSONObject();
-		if(pwd!=null) {
-			json.put("find", true);
-			json.put("pwd", pwd);
-		}else{
-			json.put("find", false);
+		req.setCharacterEncoding("utf-8");
+		String radio = req.getParameter("type");
+		if(radio.equals("일반사용자")) {
+			String id = req.getParameter("id");
+			String email = req.getParameter("email");
+			MemberDao dao = MemberDao.getInstance();
+			String pwd = dao.findPwd(id, email);
+			JSONObject json = new JSONObject();
+			if(pwd!=null) {
+				json.put("find", true);
+				json.put("pwd", pwd);
+			}else{
+				json.put("find", false);
+			}
+			resp.setContentType("text/plain;charset=utf-8");
+			PrintWriter pw = resp.getWriter();
+			pw.print(json);
+		}else if(radio.equals("판매사업자")) {
+			String num = req.getParameter("si_num");
+			String email = req.getParameter("email");
+			SellerDao dao = SellerDao.getInstance();
+			String mi_pwd = dao.sellerFindPwd(num, email);
+			JSONObject json = new JSONObject();
+			if(mi_pwd!=null) {
+				json.put("find", true);
+				json.put("pwd", mi_pwd);
+			}else {
+				json.put("find", false);
+			}
 		}
-		resp.setContentType("text/plain;charset=utf-8");
-		PrintWriter pw = resp.getWriter();
-		pw.print(json);
 	}
 }
