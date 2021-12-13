5cd205c738d92dce31f8ed3b90479f330f908020
Steel <Steel@JKim>
2021-12-13 15:04:11 +0900

    문의 관련 dao 작성

--- semiProject/src/main/java/chaneloper/dao/Inquiry_historyDao.java
+++ semiProject/src/main/java/chaneloper/dao/Inquiry_historyDao.java
@@ -4,60 +4,48 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.spi.DirStateFactory.Result;
 
 import chaneloper.vo.Inquiry_historyVo;
 import db.JDBC;
 
 public class Inquiry_historyDao {
-	// 가장 큰 글 번호
-	public int getMaxNum() {
+	
+	public ArrayList<Inquiry_historyVo> list(int startRow, int endRow){
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
 			con = JDBC.getCon();
-			String sql = "SELECT NVL(MAX(IH_NUM),0) mnum FROM INQUIRY_HISTORY";
+			String sql = "select * from "
+					+ "("
+					+ "		select ih.*, rownum rnum from"
+					+ "	    ("
+					+ "        select * from inquiry_history order by ih_num desc"
+					+ "     ) ih"
+					+ ") where rnum>=? and rnum<=?";
 			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, endRow);
 			rs = pstmt.executeQuery();
-			int maxNum = rs.getInt("mnum");
-			return maxNum;
+			ArrayList<Inquiry_historyVo> list = new ArrayList<Inquiry_historyVo>();
+			while(rs.next()) {
+				int ih_num = rs.getInt("ih_num");
+				String md_id = rs.getString("md_id");
+				int pi_num = rs.getInt("pi_num");
+				String ih_title = rs.getString("ih_title");
+				String ih_question = rs.getString("ih_question");
+				String ih_answer = rs.getString("ih_answer");
+				Inquiry_historyVo vo = new Inquiry_historyVo(ih_num, md_id, pi_num, ih_title, ih_question, ih_answer);
+				list.add(vo);
+			}
+			return list;
 		} catch(SQLException se) {
 			se.printStackTrace();
-			return -1;
-		}
-	}
-	
-	public int insert(Inquiry_historyVo vo) {
-		Connection con = null;
-		PreparedStatement pstmt1 = null;
-		PreparedStatement pstmt2 = null;
-		try {
-			con = JDBC.getCon();
-			int inquiryNum = getMaxNum() + 1;
-			
-		} catch (SQLException se) {
-			se.printStackTrace();
-			return -1;
-		}
-	}
-	
-	// 전체 글의 개수
-	public int getCount() {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			con = JDBC.getCon();
-			String sql = "SELECT NVL(COUNT(NUM),0) CNT FROM INQUIRY_HISTORY";
-			pstmt = con.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			rs.next();
-			int maxnum = rs.getInt("cnt");
-			return maxnum;			
-		} catch(SQLException se) {
-			se.printStackTrace();
-			return -1;
-		} finally {
+			return null;
+		} finally{
 			JDBC.close(con, pstmt, rs);
 		}
 	}
