0cd913befe99e5857222d6086718af120743013d
Baek SeungHan <Baek SeungHan@DESKTOP-8AH2LIL>
2021-12-13 15:30:31 +0900

    푸쉬

--- semiProject/src/main/java/chaneloper/contoller/SearchController.java
+++ semiProject/src/main/java/chaneloper/contoller/SearchController.java
@@ -22,19 +22,51 @@ public class SearchController extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		String keyword = req.getParameter("keyword");
+		String CATEGORY=req.getParameter("CATEGORY");
+		String sort=req.getParameter("sort");
+		Search_ResultDao dao=new Search_ResultDao();
 		if(keyword==null) {
 			req.setAttribute("main", "/searchResult.jsp");
+			req.setAttribute("showMsg", "검색어를 입력해주세요");
 			req.getRequestDispatcher("/layout.jsp").forward(req, resp);
 		}else {
-			// product_information Dao 조회
-			Search_ResultDao dao=new Search_ResultDao();
-			ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, null);
-			ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
-			req.setAttribute("tag", tag);
-			req.setAttribute("keyword", keyword);
-			req.setAttribute("list", list);
-			req.setAttribute("main", "/searchResult.jsp");
-			req.getRequestDispatcher("/layout.jsp").forward(req, resp);
+			if(CATEGORY==null&&sort==null) {
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, null);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("CATEGORY", CATEGORY);
+				req.setAttribute("main", "/searchResult.jsp");
+				req.getRequestDispatcher("/layout.jsp").forward(req, resp);
+			}else if(CATEGORY==null){
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, sort);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("sort", sort);
+				req.setAttribute("list", list);
+				req.setAttribute("main", "/searchResult.jsp");
+				req.getRequestDispatcher("/layout.jsp").forward(req, resp);
+			}else if(sort==null){
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, CATEGORY, null);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("CATEGORY", CATEGORY);
+				req.setAttribute("list", list);
+				req.setAttribute("main", "/searchResult.jsp");
+				req.getRequestDispatcher("/layout.jsp").forward(req, resp);
+			}else if(CATEGORY!=null&&sort!=null){
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, CATEGORY, sort);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("CATEGORY", CATEGORY);
+				req.setAttribute("sort", sort);
+				req.setAttribute("list", list);
+				req.setAttribute("main", "/searchResult.jsp");
+				req.getRequestDispatcher("/layout.jsp").forward(req, resp);
+			}
 		}
 	}
 }
--- semiProject/src/main/webapp/searchResult.jsp
+++ semiProject/src/main/webapp/searchResult.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
+<link rel="stylesheet" type="text/css" href="css.searchResult.css">
 <c:choose>
 	<c:when test="${empty requestScope.keyword }"> <!-- 검색탭으로 들어온경우 -->
 		<c:set var="keyword" value=""/>
@@ -15,21 +15,22 @@
 	<fieldset style="border:none;">
 		<form method="get" action="${cp }/search/search">
 			<select name="CATEGORY">
-				<option value="0">상품분류 선택</option>
-				<option value="1">상의</option>
-				<option value="2">하의</option>
-				<option value="3">원피스</option>
-				<option value="4">아우터</option>
-				<option value="5">악세사리</option>
-				<option value="6">검색</option>
+				<option value="0" <c:if test="${CATEGORY=='0' }">selected</c:if>>상품분류 선택</option>
+				<option value="1" <c:if test="${CATEGORY=='1' }">selected</c:if>>상의</option>
+				<option value="2" <c:if test="${CATEGORY=='2' }">selected</c:if>>하의</option>
+				<option value="3" <c:if test="${CATEGORY=='3' }">selected</c:if>>원피스</option>
+				<option value="4" <c:if test="${CATEGORY=='4' }">selected</c:if>>아우터</option>
+				<option value="5" <c:if test="${CATEGORY=='5' }">selected</c:if>>악세사리</option>
+				<option value="6" <c:if test="${CATEGORY=='6' }">selected</c:if>>검색</option>
 			</section> 
-				<input type="text" name="keyword" value="${keyword }"><br>
+				<input type="text" name="keyword" value="${keyword }" placeholder="상품명"><br>
 			<select name="sort">
-				<option value="7">:::기준선택:::</option>
-				<option value="8">신상품 순</option>
-				<option value="9">인기상품 순</option>
+				<option value="7" <c:if test="${sort=='7' }">selected</c:if>>:::기준선택:::</option>
+				<option value="8" <c:if test="${sort=='8' }">selected</c:if>>신상품 순</option>
+				<option value="9" <c:if test="${sort=='9' }">selected</c:if>>인기상품 순</option>
 			</section><br>
-			<input type="submit" value="SEARCH"> 
+			<input type="submit" value="SEARCH">
+			<input type="reset" value="초기화"> 
 		</form>
 	</fieldset>
 </div>
