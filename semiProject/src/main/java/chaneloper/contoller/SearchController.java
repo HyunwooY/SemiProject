cf2855ab2656028981dd85837c920eee3462cf32
lamgu <lamgu@lamgul>
2021-12-13 09:59:50 +0900

    리뷰 vo업데이트 및 서치 컨트롤러와 dao 수정

--- semiProject/src/main/java/chaneloper/contoller/SearchController.java
+++ semiProject/src/main/java/chaneloper/contoller/SearchController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import chaneloper.dao.Search_ResultDao;
 import chaneloper.vo.Search_ProductVo;
+import chaneloper.vo.TagVo;
 
 
 @WebServlet("/search/search")  
@@ -28,6 +29,8 @@ public class SearchController extends HttpServlet{
 			// product_information Dao 조회
 			Search_ResultDao dao=new Search_ResultDao();
 			ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, null);
+			ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
+			req.setAttribute("tag", tag);
 			req.setAttribute("keyword", keyword);
 			req.setAttribute("list", list);
 			req.setAttribute("main", "/searchResult.jsp");
--- semiProject/src/main/java/chaneloper/vo/Search_DetailProductVo.java
+++ semiProject/src/main/java/chaneloper/vo/Search_DetailProductVo.java
@@ -1,124 +1,21 @@
 package chaneloper.vo;
 
 public class Search_DetailProductVo {
-	private int pi_num;
-	private String pi_name;
-	private int pi_price;
-	private String pd_size;
-	private String pd_color;
-	private int pd_count;
-	private String pp_title;
-	private int r_num;
-	private String r_title;
-	private String r_content;
-	private String r_date;
-	private int r_hit;
-	private String rp_title;
 	private int ih_num;
-	private String mi_id;
+	private String md_id;
+	private int pi_num;
 	private String ih_title;
-	private String ih_quest;
+	private String ih_question;
 	private String ih_answer;
-	public Search_DetailProductVo(int pi_num, String pi_name, int pi_price, String pd_size, String pd_color,
-			int pd_count, String pp_title, int r_num, String r_title, String r_content, String r_date, int r_hit,
-			String rp_title, int ih_num, String mi_id, String ih_title, String ih_quest, String ih_answer) {
+	public Search_DetailProductVo(int ih_num, String md_id, int pi_num, String ih_title, String ih_question,
+			String ih_answer) {
 		super();
-		this.pi_num = pi_num;
-		this.pi_name = pi_name;
-		this.pi_price = pi_price;
-		this.pd_size = pd_size;
-		this.pd_color = pd_color;
-		this.pd_count = pd_count;
-		this.pp_title = pp_title;
-		this.r_num = r_num;
-		this.r_title = r_title;
-		this.r_content = r_content;
-		this.r_date = r_date;
-		this.r_hit = r_hit;
-		this.rp_title = rp_title;
 		this.ih_num = ih_num;
-		this.mi_id = mi_id;
-		this.ih_title = ih_title;
-		this.ih_quest = ih_quest;
-		this.ih_answer = ih_answer;
-	}
-	public int getPi_num() {
-		return pi_num;
-	}
-	public void setPi_num(int pi_num) {
+		this.md_id = md_id;
 		this.pi_num = pi_num;
-	}
-	public String getPi_name() {
-		return pi_name;
-	}
-	public void setPi_name(String pi_name) {
-		this.pi_name = pi_name;
-	}
-	public int getPi_price() {
-		return pi_price;
-	}
-	public void setPi_price(int pi_price) {
-		this.pi_price = pi_price;
-	}
-	public String getPd_size() {
-		return pd_size;
-	}
-	public void setPd_size(String pd_size) {
-		this.pd_size = pd_size;
-	}
-	public String getPd_color() {
-		return pd_color;
-	}
-	public void setPd_color(String pd_color) {
-		this.pd_color = pd_color;
-	}
-	public int getPd_count() {
-		return pd_count;
-	}
-	public void setPd_count(int pd_count) {
-		this.pd_count = pd_count;
-	}
-	public String getPp_title() {
-		return pp_title;
-	}
-	public void setPp_title(String pp_title) {
-		this.pp_title = pp_title;
-	}
-	public int getR_num() {
-		return r_num;
-	}
-	public void setR_num(int r_num) {
-		this.r_num = r_num;
-	}
-	public String getR_title() {
-		return r_title;
-	}
-	public void setR_title(String r_title) {
-		this.r_title = r_title;
-	}
-	public String getR_content() {
-		return r_content;
-	}
-	public void setR_content(String r_content) {
-		this.r_content = r_content;
-	}
-	public String getR_date() {
-		return r_date;
-	}
-	public void setR_date(String r_date) {
-		this.r_date = r_date;
-	}
-	public int getR_hit() {
-		return r_hit;
-	}
-	public void setR_hit(int r_hit) {
-		this.r_hit = r_hit;
-	}
-	public String getRp_title() {
-		return rp_title;
-	}
-	public void setRp_title(String rp_title) {
-		this.rp_title = rp_title;
+		this.ih_title = ih_title;
+		this.ih_question = ih_question;
+		this.ih_answer = ih_answer;
 	}
 	public int getIh_num() {
 		return ih_num;
@@ -126,11 +23,17 @@ public class Search_DetailProductVo {
 	public void setIh_num(int ih_num) {
 		this.ih_num = ih_num;
 	}
-	public String getMi_id() {
-		return mi_id;
+	public String getMd_id() {
+		return md_id;
 	}
-	public void setMi_id(String mi_id) {
-		this.mi_id = mi_id;
+	public void setMd_id(String md_id) {
+		this.md_id = md_id;
+	}
+	public int getPi_num() {
+		return pi_num;
+	}
+	public void setPi_num(int pi_num) {
+		this.pi_num = pi_num;
 	}
 	public String getIh_title() {
 		return ih_title;
@@ -138,11 +41,11 @@ public class Search_DetailProductVo {
 	public void setIh_title(String ih_title) {
 		this.ih_title = ih_title;
 	}
-	public String getIh_quest() {
-		return ih_quest;
+	public String getIh_question() {
+		return ih_question;
 	}
-	public void setIh_quest(String ih_quest) {
-		this.ih_quest = ih_quest;
+	public void setIh_question(String ih_question) {
+		this.ih_question = ih_question;
 	}
 	public String getIh_answer() {
 		return ih_answer;
