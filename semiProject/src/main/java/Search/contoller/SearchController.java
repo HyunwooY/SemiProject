15a12587103b2836d94ecdafc8aa33e942480b07
lamgu <lamgu@lamgul>
2021-12-14 13:08:03 +0900

    searchresult.jsp 업데이트

--- semiProject/src/main/java/Search/contoller/SearchController.java
+++ semiProject/src/main/java/Search/contoller/SearchController.java
@@ -22,51 +22,68 @@ public class SearchController extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		String keyword = req.getParameter("keyword");
-		String CATEGORY=req.getParameter("CATEGORY");
-		String sort=req.getParameter("sort");
-		Search_ResultDao dao=new Search_ResultDao();
-		
-		if(keyword==null) {
-			req.setAttribute("main", "/searchResult.jsp");
-			req.getRequestDispatcher("/layout.jsp").forward(req, resp);
-		}else if(CATEGORY==null&&sort.equals("null")) {
-			ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, null);
-			ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
-			req.setAttribute("tag", tag);
-			req.setAttribute("keyword", keyword);
-			req.setAttribute("list", list);
-			req.setAttribute("main", "/searchResult.jsp");
-			req.getRequestDispatcher("/layout.jsp").forward(req, resp);
-		}else if(CATEGORY==null) {
-			ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, sort);
-			ArrayList<TagVo> tag = dao.get_tag(keyword, null, sort);
-			req.setAttribute("tag", tag);
-			req.setAttribute("keyword", keyword);
-			req.setAttribute("sort", sort);
-			req.setAttribute("list", list);
-			req.setAttribute("main", "/searchResult.jsp");
-			req.getRequestDispatcher("/layout.jsp").forward(req, resp);
-		}else if(sort.equals("null")) {
-			ArrayList<Search_ProductVo> list=dao.search_product(keyword, CATEGORY, null);
-			ArrayList<TagVo> tag = dao.get_tag(keyword, CATEGORY, null);
-			req.setAttribute("tag", tag);
-			req.setAttribute("keyword", keyword);
-			req.setAttribute("CATEGORY", CATEGORY);
-			req.setAttribute("list", list);
-			req.setAttribute("main", "/searchResult.jsp");
-			req.getRequestDispatcher("/layout.jsp").forward(req, resp);
-		}else if(CATEGORY!=null&&sort.equals("null")) {
-			ArrayList<Search_ProductVo> list=dao.search_product(keyword, CATEGORY, sort);
-			ArrayList<TagVo> tag = dao.get_tag(keyword, CATEGORY, sort);
-			req.setAttribute("tag", tag);
-			req.setAttribute("keyword", keyword);
-			req.setAttribute("CATEGORY", CATEGORY);
-			req.setAttribute("sort", sort);
-			req.setAttribute("list", list);
-			req.setAttribute("main", "/searchResult.jsp");
-			req.getRequestDispatcher("/layout.jsp").forward(req, resp);
+
+		String category =req.getParameter("CATEGORY");
+		if(category!=null) {
+			if(category.equals("1")) {
+				category=null;
 			}
 		}
+		String sort = req.getParameter("sort");
+		if(sort!=null) {
+			if(sort.equals("1")) {
+				sort=null;
+			}
+		}
+
+		Search_ResultDao dao=new Search_ResultDao();
+		
+		//검색탭으로 검색할때
+		
+		
+		
+
+		//세부페이지로 이동후 검색할때
+		if(keyword!=null) {
+			//카테고리랑 정렬이 없다면
+			if(category==null&&sort==null) {
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, null);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, null, null);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("list", list);
+			//카테고리만 없다면
+			}else if(category==null) {
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, null, sort);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, null, sort);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("sort", sort);
+				req.setAttribute("list", list);
+			//정렬만 없다면
+			}else if(sort==null) {
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, category, null);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, category, null);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("CATEGORY", category);
+				req.setAttribute("list", list);
+			// 둘다 있을때
+			}else if(category!=null&&sort!=null) {
+				ArrayList<Search_ProductVo> list=dao.search_product(keyword, category, sort);
+				ArrayList<TagVo> tag = dao.get_tag(keyword, category, sort);
+				req.setAttribute("tag", tag);
+				req.setAttribute("keyword", keyword);
+				req.setAttribute("CATEGORY", category);
+				req.setAttribute("sort", sort);
+				req.setAttribute("list", list);
+
+			}
+
+		}
+		req.setAttribute("main", "/search/searchResult.jsp");
+		req.getRequestDispatcher("/layout.jsp").forward(req, resp);
 	}
+}
 
 
