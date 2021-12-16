f2f37932966dcee1d9e87d19b27b644e2bc14c6b
Steel <Steel@JKim>
2021-12-16 23:46:15 +0900

    문의 게시판 수정

--- semiProject/src/main/java/chaneloper/seller/controller/ProductDeleteController.java
+++ semiProject/src/main/java/chaneloper/seller/controller/ProductDeleteController.java
@@ -11,24 +11,24 @@ import javax.servlet.http.HttpServletResponse;
 import chaneloper.dao.ProductDao;
 import chaneloper.vo.ProductVo;
 
-@WebServlet("/seller/delete")
-public class ProductDeleteController extends HttpServlet{
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-
-		int pi_num = Integer.parseInt(req.getParameter("pi_num"));	
-		
-		ProductDao dao = ProductDao.getInstance();
-		
-		int n= dao.productDelete(pi_num);
-		if(n>0) {
-			req.setAttribute("productcode", "success");
-			req.setAttribute("main", "/seller/sellerpage.jsp");
-		}else {
-			req.setAttribute("productcode", "fail");
-			req.setAttribute("main", "/seller/sellerpage.jsp");
-		}
-		req.getRequestDispatcher("/layout.jsp").forward(req, resp);
-	}
-}
+//@WebServlet("/seller/#")
+//public class ProductDeleteController extends HttpServlet{
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		req.setCharacterEncoding("UTF-8");
+//
+//		int pi_num = Integer.parseInt(req.getParameter("pi_num"));	
+//		
+//		ProductDao dao = ProductDao.getInstance();
+//		
+//		int n= dao.productDelete(pi_num);
+//		if(n>0) {
+//			req.setAttribute("productcode", "success");
+//			req.setAttribute("main", "/seller/#.jsp");
+//		}else {
+//			req.setAttribute("productcode", "fail");
+//			req.setAttribute("main", "/seller/#.jsp");
+//		}
+//		req.getRequestDispatcher("/layout.jsp").forward(req, resp);
+//	}
+//}
