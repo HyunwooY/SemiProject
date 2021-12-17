60eef122eae75e5c38c26afb8b70931b80e1c4cf
yhw92 <yhw92@220.121.11.125>
2021-12-17 16:13:52 +0900

    .

--- semiProject/src/main/java/chaneloper/member/controller/PayinfoController.java
+++ semiProject/src/main/java/chaneloper/member/controller/PayinfoController.java
@@ -1,6 +1,7 @@
 package chaneloper.member.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,18 +20,13 @@ public class PayinfoController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String id=(String)req.getSession().getAttribute("id");
-		String pd_num=(String)req.getSession().getAttribute("pd_num");
-		int num=-1;
-		if(pd_num==null||pd_num=="") {
-			num=0;
-		}else {
-			num=Integer.parseInt(pd_num);
-		}
+		HashMap<Integer, Integer> pd_num=(HashMap<Integer, Integer>)req.getSession().getAttribute("pd_num");
+		System.out.println(pd_num);
 		MemberDao dao=MemberDao.getInstance();
 		PurchaseDao pdao=PurchaseDao.getInstance();
 		MemberVo membervo=dao.select(id);
 		AddressVo addressvo=dao.selectaddr(id, membervo.getName());
-		ShowPurchaseListVo svo=pdao.selectProduct(num);
+		ShowPurchaseListVo svo=pdao.selectProduct(1);
 		req.setAttribute("vo", membervo);
 		req.setAttribute("avo", addressvo);
 		req.setAttribute("svo", svo);
