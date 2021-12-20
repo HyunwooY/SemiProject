80909debe63913c1d654ef757321e2d742672cd8
slsl7 <slsl7@DESKTOP-C41FD0M>
2021-12-21 00:13:30 +0900

    수정필요

---
+++ semiProject/src/main/java/chaneloper/member/controller/AddrManagementController.java
@@ -0,0 +1,29 @@
+package chaneloper.member.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import chaneloper.dao.MemberDao;
+import chaneloper.vo.AddressVo;
+@WebServlet("/member/addrmanagement")
+public class AddrManagementController extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		req.setCharacterEncoding("utf-8");
+		String id = (String)req.getSession().getAttribute("id");
+		MemberDao dao = MemberDao.getInstance();
+		ArrayList<AddressVo> list = dao.addrList(id);
+		req.setAttribute("list", list);
+		
+		req.setAttribute("detailmain", "/member/insertAddress.jsp");
+		req.setAttribute("main", "/member/mDetail.jsp");
+		
+		req.getRequestDispatcher("/member/memberDetail").forward(req, resp);
+	}
+}
