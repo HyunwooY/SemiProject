65f253066f7ef909a204ec20901cd8a0b913aa82
Steel <Steel@JKim>
2021-12-23 23:49:00 +0900

    푸쉬

--- semiProject/src/main/java/chaneloper/seller/controller/InquiryHistoryListController.java
+++ semiProject/src/main/java/chaneloper/seller/controller/InquiryHistoryListController.java
@@ -27,10 +27,10 @@ public class InquiryHistoryListController extends HttpServlet{
 		int endRow = startRow + 9;
 		
 		Inquiry_historyDao dao = Inquiry_historyDao.getInstance();
-		ArrayList<Inquiry_historyVo> list = dao.list(startRow, endRow, null, null);
+		ArrayList<Inquiry_historyVo> list = dao.list(startRow, endRow, null, null, null);
 		
 		
-		int count = dao.getCount(null, null);		// 전체 글의 수
+		int count = dao.getCount(null, null, null);		// 전체 글의 수
 		int pageCount = (int)Math.ceil(count / 10.0);		// 전체 페이지 수
 		int startPageNum = ((pageNum - 1) / 10 * 10) + 1;		// 시작 페이지 번호
 		int endPageNum = startPageNum + 9;		// 끝 페이지 번호
